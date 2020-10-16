abde3b6090faca5151a4d7209276a9f1448f6a57
Dmitriy <itsmalevich@gmail.com>
2020-10-16 12:07:14 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import Total from './components/total/Total'
+import History from './components/history/History'
+import Operation from './components/operation/Operation'
+
+class App extends Component {
+
+  state = {
+    transactions: [],
+    description: '',
+    amount: '',
+  }
+/*  //то же что и сверху
+  constructor (props) {
+    super(props);
+    this.state = {
+      transaction: [],
+    }
+  }
+*/
+
+  addTransaction = add => {
+/*
+    const transactions = [...this.state.transactions];
+
+    const transaction = {
+      id: `cmr${(+new Date).toString(16)}`,
+      description: this.state.description,
+      amount: this.state.amount,
+      add
+    }
+    transactions.push(transaction);
+*/ //сокращение 
+
+    const transactions = [...this.state.transactions, 
+      {
+        id: `cmr${(+new Date).toString(16)}`,
+        description: this.state.description,
+        amount: this.state.amount,
+        add
+      }
+    ];
+
+    this.setState({
+      transactions,
+      description: '',
+      amount: '',
+    });
+  }
+
+  addAmount = e => {
+    //this.state.amount = e.target.value; //будет обновлятся но не будет происходить перерендера
+    this.setState({amount: e.target.value});
+  }
+  addDescription = e => {
+    this.setState({description: e.target.value});
+  }
+
+  render() {
+    return (
+      <>
+      <header>
+        <h1>Кошелек</h1>
+        <h2>Калькулятор расходов</h2>
+      </header>
+  
+      <main>
+        <div className="container">
+          <Total/>  
+          <History transactions={this.state.transactions}/>  
+          <Operation 
+          addTransaction={this.addTransaction} 
+          addAmount={this.addAmount}
+          addDescription={this.addDescription}
+          description={this.state.description}
+          amount={this.state.amount}
+          />
+        </div>
+      </main>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/history/History.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import HistoryItem from './HistoryItem';
+
+export default function({transactions}) {
+  return (
+    <section className="history">
+      <h3>История расходов</h3>
+      <ul className="history__list">
+        {transactions.map(item => <HistoryItem 
+          key={item.id} 
+          transaction={item}
+        />)}
+      </ul>
+    </section>
+  )
+}
